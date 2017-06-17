0d5a629437bc49dfbeb0f9ee94d0f670a3ddc310
AnujaySaraf <AnujaySaraf@users.noreply.github.com>
2017-06-17 01:31:39 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,6 @@
 def solution(dic):
-    """Enter Code Here"""
+    """Shortcut to get the list of keys in sorted order from dictionary i.e sort on keys"""
+    print dic.keys()
+    return dic.keys()
+
+solution({1: 10, 4: 40, 2: 20, 3: 30})
